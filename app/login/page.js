fe7ef3042b55bbbdc2fd74784da155b94091bca7
Ryan Cobourn <rjcobourn@gmail.com>
2023-11-30 17:12:57 -0700

    Rough login page mockup

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,40 @@
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div class="login-page-container">
+      <div class="login-ui">
+        <form action="/submit-your-login-form" method="post">
+          <div class="form-group">
+            <label class="primary-color display-block" for="username">
+              Username:
+            </label>
+            <input
+              class="login-input"
+              type="text"
+              id="username"
+              name="username"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <label class="primary-color display-block" for="password">
+              Password:
+            </label>
+            <input
+              class="login-input"
+              type="password"
+              id="password"
+              name="password"
+              required
+            />
+          </div>
+          <button class="login-page-buttons" type="submit">
+            Login
+          </button>
+          <button class="login-page-buttons" type="submit">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
