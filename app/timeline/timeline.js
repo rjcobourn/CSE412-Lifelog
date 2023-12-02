cdc8ab36d0025b0e7d35d23a3bf0038c56c9593d
Brad Peterson <86531889+brpeterson@users.noreply.github.com>
2023-12-02 01:58:47 -0700

    Added simple form modal. Does not work yet.

--- app/timeline/timeline.js
+++ app/timeline/timeline.js
@@ -40,10 +40,35 @@ const addButtonStyle = {
 	// Add other styles like background color, hover effects, etc.
 };
 
+
+const modalBackdropStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    zIndex: 1000,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+	backdropFilter: 'blur(5px)',
+};
+
+const modalStyle = {
+    backgroundColor: 'white',
+    padding: '20px',
+    borderRadius: '5px',
+    zIndex: 1001,
+    // Add more styling as needed...much more needed
+};
+
+
+
 const timelineEvents = [
-	{ id: 1, title: 'Event 1', date: '2023-01-01' },
-	{ id: 2, title: 'Event 2', date: '2023-01-02' },
-	{ id: 3, title: 'Event 3', date: '2023-01-03' },
+	{ id: 1, title: 'Event 1', date: '2023-01-01', text: '', image: null },
+	{ id: 2, title: 'Event 2', date: '2023-01-02', text: '', image: null },
+	{ id: 3, title: 'Event 3', date: '2023-01-03', text: '', image: null },
 	// { id: 4, title: 'Event 4', date: '2023-01-04' },
 	// { id: 5, title: 'Event 5', date: '2023-01-05' },
 	// { id: 6, title: 'Event 6', date: '2023-01-06' },
@@ -59,11 +84,31 @@ const Timeline = () => {
     const [newEventId, setNewEventId] = useState(timelineEvents.length + 1);
     const eventRefs = useRef({});
 
-    const handleAddEvent = () => {
+	const [showForm, setShowForm] = useState(false);
+	const [newEventTitle, setNewEventTitle] = useState('');
+	const [newEventDate, setNewEventDate] = useState('');
+	// const [newEventType, setNewEventType] = useState(''); // 'Text Entry' or other types
+	const [newEventText, setNewEventText] = useState('');
+	const [newEventImage, setNewEventImage] = useState(null);
+	const [newEventTags, setNewEventTags] = useState('');
+
+
+	
+	const handleImageUpload = (e) => {
+		const file = e.target.files[0];
+		if (file) {
+			// validation here (e.g., file type, file size)
+			setNewEventImage(file);
+		}
+	};
+
+    const handleAddEvent = (title, date, text, image) => {
         const newEvent = {
             id: newEventId,
-            title: `Event ${newEventId}`,
-            date: '2023-01-10',
+            title,
+            date,
+            text,
+            image: image ? URL.createObjectURL(image) : null,
         };
 
         setEvents([...events, newEvent]);
@@ -77,22 +122,73 @@ const Timeline = () => {
         }, 0);
     };
 
+	const handleSubmit = async () => {
+		const formData = new FormData();
+		formData.append('title', newEventTitle);
+		formData.append('date', newEventDate);
+		formData.append('text', newEventText);
+		formData.append('tags', newEventTags);
+		if (newEventImage) {
+			formData.append('image', newEventImage);
+		}
+	
+		try {
+			// Send data to the server
+			const response = await fetch('/api/route', {
+				method: 'POST',
+				body: formData,
+			});
+	
+			const data = await response.json();
+			if (data.message === "Success") {
+				// Process success response, update state, etc.
+				handleAddEvent(newEventTitle, newEventDate, newEventText, newEventImage);
+				// Reset form and close it
+				setNewEventTitle('');
+				setNewEventDate('');
+				setNewEventText('');
+				setNewEventImage(null);
+				setShowForm(false);
+			} else {
+				// Handle error response
+				console.error(data.error);
+			}
+		} catch (error) {
+			// Handle network or other errors
+			console.error('An error occurred:', error);
+		}
+	};
+
+
     const timelineWidth = `calc(${(events.length + 1) * 50}vh + 32px)`;
 
-	
+
     return (
       <div style={{ ...timelineStyle, width: timelineWidth }}>
         {events.map(event => (
-          <div
-            key={event.id}
-            ref={el => eventRefs.current[event.id] = el}
-            style={itemStyle}
-          >
-            <h3>{event.title}</h3>
-            <p>{event.date}</p>
-          </div>
+			<div key={event.id} ref={el => eventRefs.current[event.id] = el} style={itemStyle}>
+			<h3>{event.title}</h3>
+			<p>{event.date}</p>
+			{event.text && <p>{event.text}</p>}
+			{event.image && <img src={event.image} alt={event.title} style={{ width: '100%', height: 'auto' }} />}
+	  </div>
         ))}
-        <div style={addButtonStyle} onClick={handleAddEvent}>+</div>
+        <div style={addButtonStyle} onClick={() => setShowForm(true)}>+</div>
+
+		{showForm && (
+			<div style={modalBackdropStyle}>
+				<div style={modalStyle}>
+					<input type="text" placeholder="Title" value={newEventTitle} onChange={e => setNewEventTitle(e.target.value)} />
+					<input type="date" value={newEventDate} onChange={e => setNewEventDate(e.target.value)} />
+					<textarea placeholder="Text" value={newEventText} onChange={e => setNewEventText(e.target.value)} />
+					<input type="file" onChange={handleImageUpload} />
+					<input type="text" placeholder="Tags" value={newEventTags} onChange={e => setNewEventTags(e.target.value)} />
+					<button onClick={handleSubmit}>Submit</button>
+					<button onClick={() => setShowForm(false)}>Cancel</button>
+				</div>
+			</div>
+		)}
+
       </div>
     );
 };
