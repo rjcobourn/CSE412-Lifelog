3e1ebb6cf49b1a25b696ff5fc5f27959db32d27d
Yash Sarkar <yashsarkar@MacBook-Air.local>
2023-12-02 22:24:43 -0700

    Added Notyf Error handling

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,21 @@
 "use client";
+import React, { useEffect, useState } from 'react';
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css'; // for styles
+
 export default function Login() {
+  const [notyf, setNotyf] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const notyfInstance = new Notyf({
+        duration: 8000, // Duration in milliseconds, e.g., 5000 for 5 seconds
+        // You can add other Notyf options here
+      });
+      setNotyf(notyfInstance);
+    }
+  }, []);
+
   const handleSignUp = async () => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -12,15 +28,15 @@ export default function Login() {
         },
         body: JSON.stringify({ username, password }),
       });
+
       if (response.ok) {
         window.location.href = "/";
       } else {
-        // Sign up failed, display the error
-        console.error("Error during sign up:", response.statusText);
-        // Add some code here to display the error to the user
+        const errorText = await response.text();
+        if (notyf) handleErrorResponse(response.status, errorText, 'signup');
       }
     } catch (error) {
-      console.error("Error during sign up:", error);
+      if (notyf) notyf.error("Error during sign up: " + error.message);
     }
   };
 
@@ -36,15 +52,33 @@ export default function Login() {
         },
         body: JSON.stringify({ username, password }),
       });
+
       if (response.ok) {
         window.location.href = "/";
       } else {
-        // Login failed, display the error
-        console.error("Error during login:", response.statusText);
-        // Add some code here to display the error to the user
+        const errorText = await response.text();
+        if (notyf) handleErrorResponse(response.status, errorText, 'login');
       }
     } catch (error) {
-      console.error("Error during login:", error);
+      if (notyf) notyf.error("Error during login: " + error.message);
+    }
+  };
+
+  const handleErrorResponse = (status, message, action) => {
+    if (!notyf) return;
+
+    switch (status) {
+      case 401:
+        notyf.error("Incorrect username or password.");
+        break;
+      case 400:
+        notyf.error("Username already exists.");
+        break;
+      case 500:
+        notyf.error("Internal Server Error: Please try again later.");
+        break;
+      default:
+        notyf.error(`${action.charAt(0).toUpperCase() + action.slice(1)} failed: ${message}`);
     }
   };
 
